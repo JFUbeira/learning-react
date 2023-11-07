c92de44448ef3faab1ac5b6247e9ec5d6cc04929
JuanFUbeira <franciscoubeira@gmail.com>
2023-11-07 03:33:22 -0300

    Styled containers, background. Changed pokemons

--- src/app/ejercicios/1/page.js
+++ src/app/ejercicios/1/page.js
@@ -10,7 +10,8 @@ async function Ejercicio1Page() {
     const { results } = data
 
     // agarro 5
-    const pokemons = results.slice(0, 5)
+    // const pokemons = results.slice(0, 6)
+    const pokemons = results.filter((_, index) => [2, 5, 8].includes(index))
 
     // fetch pokemon
     // const fetchPokemon = async (url) => {
@@ -47,21 +48,27 @@ async function Ejercicio1Page() {
     const pokemonsData = await Promise.all(pokemonsToRender)
 
     return (
-        <div className="flex flex-col gap-4 p-4">
-            {pokemonsData.map((pokemon) => (
-                <div
-                    key={pokemon.name}
-                    className="border rounded-lg flex justify-center items-center w-[400px] gap-4"
-                >
-                    <img src={pokemon.image} alt="" />
+        <main className="bg-slate-900  h-screen flex justify-center items-center">
+            <div className="flex flex-col gap-4 p-4">
+                {pokemonsData.map((pokemon) => (
+                    <div
+                        key={pokemon.name}
+                        className="border rounded-lg flex justify-start items-center w-[500px] gap-4"
+                    >
+                        <img src={pokemon.image} alt="" className="pl-5" />
 
-                    <div>
-                        <h1 className="text-2xl capitalize">{pokemon.name}</h1>
-                        <p>{pokemon.experience}</p>
+                        <div>
+                            <h1 className="text-2xl capitalize text-slate-100 font-semibold">
+                                {pokemon.name}
+                            </h1>
+                            <p className="text-slate-100">
+                                {pokemon.experience}
+                            </p>
+                        </div>
                     </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+        </main>
     )
 }
 
